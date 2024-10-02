a35382c255429601fc159379ad17e78964a6cead
Guillermo Escalona Olivares <geoshada@gmail.com>
2024-10-02 15:45:06 -0600

    Se agrega la opcion con for

--- arrayNumeros/resources/js/index.js
+++ arrayNumeros/resources/js/index.js
@@ -15,12 +15,32 @@ const calculate=()=>{
     //obtenemos el promedio o media aritemetica
     let suma = arregloNumeros.reduce((acumulador, valor) => acumulador + valor, 0);
     let media= suma/arregloNumeros.length;
+    obtenerLoMismoConFor(arregloNumeros)
     //pintamos en pantalla
     listaNumeros.textContent=arregloNumeros.join(", ");
     numMayor.textContent=`El numero mayor es ${numeroMayor} `;
     numMenor.textContent=`El numero menor es ${numeroMenor} `;
     numMedia.textContent=`La media es ${media} `;
 
+}
+
+const obtenerLoMismoConFor=(array)=>{
+    let minimo=undefined;
+    let mayor=0;
+    let suma=0
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(minimo==undefined||element<minimo){
+            minimo=element
+        }
+        if(element>mayor){
+            mayor=element;
+        }
+        suma=suma+element;
+    }
+    console.log("El valo minimo es:"+minimo)
+    console.log("El valo minimo es:"+mayor)
+    console.log("El valo media es:"+(suma/array.length))
 }
 
 //genera un arreglo de numeros enteros aleatorios
